5cffc25f6d825fb575dbbce8e4e721e49cfab41e
Vivek Waldia <vivekwaldia71@gmail.com>
2025-11-02 15:06:25 +0530

    Make proximity ranges dynamic and add better directional hints

--- game.c
+++ game.c
@@ -6,102 +6,96 @@
 #define MEDIUM_MAX 100
 #define HARD_MAX 500
 
-void playGame(int min, int max){
+void playGame(int min, int max) {
     int answer = (rand() % (max - min + 1)) + min;
     int guess = 0, tries = 0;
     int difference;
 
     printf("Guess the number between %d and %d\n\n", min, max);
 
-    while(1){
+    int range = max - min;
+    int veryClose = range / 50;   // about 2% of range
+    int close = range / 20;       // about 5% of range
+    int warm = range / 10;        // about 10% of range
+    int far = range / 5;          // about 20% of range
+
+    while (1) {
         printf("Enter your guess: ");
-        
-        if(scanf("%d", &guess) != 1){
+
+        if (scanf("%d", &guess) != 1) {
             printf("Invalid input! Enter numbers only.\n");
-            while(getchar() != '\n');
+            while (getchar() != '\n');
             continue;
         }
 
         tries++;
 
-        if(guess < min || guess > max){
+        if (guess < min || guess > max) {
             printf("Out of range! Please enter between %d and %d.\n", min, max);
             continue;
         }
 
-        if(guess == answer){
+        if (guess == answer) {
             printf("CORRECT!\n");
-            if(tries == 1){
+            if (tries == 1)
                 printf("You guessed it in 1 try.\n\n");
-            }else{
+            else
                 printf("You guessed it in %d tries.\n\n", tries);
-            }
             break;
         }
 
         difference = abs(guess - answer);
-        
-        if(difference == 1){
-            if(guess > answer){
-                printf("SO CLOSE! Just 1 below!\n");
-            }else{
-                printf("SO CLOSE! Just 1 above!\n");
-            }
-        }else if(difference <= 5){
-            if(guess > answer){
-                printf("Very close! Within 5 below.\n");
-            }else{
-                printf("Very close! Within 5 above.\n");
-            }
-        }else if(difference <= 10){
-            if(guess > answer){
-                printf("Getting warm! Within 10 below.\n");
-            }else{
-                printf("Getting warm! Within 10 above.\n");
-            }
-        }else if(difference <= 20){
-            if(guess > answer){
-                printf("Warm! Within 20 below.\n");
-            }else{
-                printf("Warm! Within 20 above.\n");
-            }
-        }else if(difference <= 50){
-            if(guess > answer){
-                printf("You're in the neighborhood.\n");
-            }else{
-                printf("You're in the neighborhood.\n");
-            }
-        }else{
-            if(guess > answer){
-                printf("TOO HIGH! Way off!\n");
-            }else{
-                printf("TOO LOW! Way off!\n");
-            }
+
+        if (difference <= veryClose) {
+            if (guess > answer)
+                printf("Super close! Try a slightly lower number.\n");
+            else
+                printf("Super close! Try a slightly higher number.\n");
+        } else if (difference <= close) {
+            if (guess > answer)
+                printf("Very close. Try a lower number.\n");
+            else
+                printf("Very close. Try a higher number.\n");
+        } else if (difference <= warm) {
+            if (guess > answer)
+                printf("Getting warm. Try a lower number.\n");
+            else
+                printf("Getting warm. Try a higher number.\n");
+        } else if (difference <= far) {
+            if (guess > answer)
+                printf("Somewhat far. Try a lower number.\n");
+            else
+                printf("Somewhat far. Try a higher number.\n");
+        } else {
+            if (guess > answer)
+                printf("Way off. Enter a much lower number.\n");
+            else
+                printf("Way off. Enter a much higher number.\n");
         }
     }
 }
 
-int main(){
+int main() {
     srand(time(NULL));
 
     int choice, playAgain = 1;
 
     printf("==== NUMBER GUESSING GAME ====\n\n");
 
-    while(playAgain == 1){
+    while (playAgain == 1) {
         printf("Select difficulty level:\n");
         printf("1. Easy (0-%d)\n", EASY_MAX);
         printf("2. Medium (0-%d)\n", MEDIUM_MAX);
         printf("3. Hard (0-%d)\n", HARD_MAX);
         printf("Enter choice (1-3): ");
 
-        if(scanf("%d", &choice) != 1){
+        if (scanf("%d", &choice) != 1) {
             printf("Invalid input! Enter numbers only.\n\n");
-            while(getchar() != '\n');
+            while (getchar() != '\n');
             continue;
         }
 
-        switch(choice){
+        switch (choice) {
             case 1:
                 playGame(0, EASY_MAX);
                 break;
@@ -116,29 +110,27 @@ int main(){
                 continue;
         }
 
-        while(1){
+        while (1) {
             printf("Do you want to play again?\n");
             printf("1. Yes\n");
             printf("2. No\n");
             printf("Enter choice: ");
             
-            if(scanf("%d", &playAgain) != 1){
+            if (scanf("%d", &playAgain) != 1) {
                 printf("Invalid input! Enter 1 or 2.\n\n");
-                while(getchar() != '\n');
+                while (getchar() != '\n');
                 continue;
             }
             
-            if(playAgain == 1 || playAgain == 2){
+            if (playAgain == 1 || playAgain == 2)
                 break;
-            }
-            
+
             printf("Invalid choice! Enter 1 or 2.\n\n");
         }
-        
+
         printf("\n");
     }
 
     printf("Thank you for playing! :)\n");
-
     return 0;
 }
